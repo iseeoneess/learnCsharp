36dee68de4209520f5c4df28deca50b1bb19164d
iseeeoneess <iseeone@mail.ru>
2022-03-16 02:21:18 +0300

    Сделал случайно калькулятор сложения

--- Program.cs
+++ Program.cs
@@ -6,13 +6,26 @@ namespace LearnCSharp
     public class Program
     {
         static void Main(string[] args)
+
         {
-            string data;
+            Console.WriteLine("Введите число 1:");
 
-            data = Console.ReadLine();
-            string xueta = " aga";
+            string num1 = Console.ReadLine();
 
-            Console.WriteLine(data + xueta);
+            int convert1 = Convert.ToInt32(num1);
+
+
+            Console.WriteLine("Введите число 2:");
+
+            string num2 = Console.ReadLine();
+
+            int convert2 = Convert.ToInt32(num2);
+
+
+            int sum = convert1 + convert2;
+
+            Console.WriteLine("Сумма чисел: " + sum);
+
 
         }
     }
