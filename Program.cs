2cee8748a92798b55d7635f562301bdcce9a7e84
iseeeoneess <iseeone@mail.ru>
2022-03-17 20:32:25 +0300

    TestCommit

--- Program.cs
+++ Program.cs
@@ -8,29 +8,21 @@ namespace LearnCSharp
         static void Main(string[] args)
         {
             // Среднее арифметическое двух чисел
-            Console.WriteLine("Данная программа вычисляет С.А двух чисел.");
-            
-            double firstNum, secondNum, thirdNum;
-
-
             Console.WriteLine("Введите первое число: ");
 
-            firstNum = double.Parse(Console.ReadLine());
+            double firstNum = double.Parse(Console.ReadLine());
 
+            Console.WriteLine("Введите второе число: ");
 
-            Console.WriteLine("Введите второе число");
-
-            secondNum = double.Parse(Console.ReadLine());
-
+            double secondNum = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Введите третье число");
 
-            thirdNum = double.Parse(Console.ReadLine());
-
-
+            double thirdNum = double.Parse(Console.ReadLine());
+            
             double result = (firstNum + secondNum + thirdNum) / 3;
 
-            Console.WriteLine("Среднее арифметическое этих чисел = " + result);
+            Console.WriteLine("Среднее арифметическое данных чисел = " + result);
 
         }
     }
