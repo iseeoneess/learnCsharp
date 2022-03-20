f33e762a733f3da6b92ca082b71e9078d8ecf5bd
iseeeoneess <iseeone@mail.ru>
2022-03-20 23:28:00 +0300

    Диапазон четных и нечетных чисел.

--- Program.cs
+++ Program.cs
@@ -7,39 +7,48 @@ namespace LearnCSharp
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Ты гуль?");
-			int i = 1007;
-			int aboba = 1000;
-			string ghoul = Console.ReadLine();
-			if (ghoul == "я гуль" || ghoul == "Я гуль")
-			{
-				while (i > 0)
-				{
-					if (i == 6)
-					{
-						int j = 40;
-						
-						while (j > 0)
-                        {
-							j--;
-							Thread.Sleep(30);
-							Console.WriteLine("Ты долбаееб.Ф_В)Ф_+");
-							continue;
-                        }
-						break;
-					}
-					else
-					{
-						i -= 7;
-						aboba -= 7;
-						Thread.Sleep(17);
+			int evenNumber = 0,
+				 oddNumber = 0;
 
-						string result = i.ToString() + " - 7" + " = " + aboba.ToString();
-						Console.WriteLine(result);
+
+
+			int evenNumberSums = 0,
+				oddNumberSums = 0;
+
+
+
+			Console.WriteLine("Введите первое число диапазона\n");
+
+			int currentValue = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Введите второе число диапазона\n");
+
+			int limitValue = int.Parse(Console.ReadLine());
+
+			
+				while (currentValue <= limitValue)
+                {
+					currentValue++;
+
+					if (currentValue % 2 == 0)
+					{
+						evenNumber++;
+						evenNumberSums += currentValue;
+					}
+
+					else if (currentValue % 2 == 1)
+					{
+						oddNumber++;
+						oddNumberSums += currentValue;
 					}
 				}
-			}
 
+				 
+			Console.WriteLine("Четных чисел в диапазоне:\n" + evenNumber);
+			Console.WriteLine("Нечетных чисел в диапазоне:\n" + oddNumber);
+
+			Console.WriteLine("Сумма четных чисел: " + evenNumberSums);
+			Console.WriteLine("Сума нечетных чисел: " + oddNumberSums);
 		}
 	}
 }
