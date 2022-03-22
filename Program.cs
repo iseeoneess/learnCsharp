9d80e8e1a79b5cc52e96ad8966e741a863aa8ee9
iseeeoneess <iseeone@mail.ru>
2022-03-22 17:08:14 +0300

    Xue-ta

--- Program.cs
+++ Program.cs
@@ -7,48 +7,15 @@ namespace LearnCSharp
 	{
 		static void Main(string[] args)
 		{
-			int evenNumber = 0,
-				 oddNumber = 0;
+            // Цикл for...
 
 
 
-			int evenNumberSums = 0,
-				oddNumberSums = 0;
 
-
-
-			Console.WriteLine("Введите первое число диапазона\n");
-
-			int currentValue = int.Parse(Console.ReadLine());
-
-			Console.WriteLine("Введите второе число диапазона\n");
-
-			int limitValue = int.Parse(Console.ReadLine());
-
-			
-				while (currentValue <= limitValue)
-                {
-					currentValue++;
-
-					if (currentValue % 2 == 0)
-					{
-						evenNumber++;
-						evenNumberSums += currentValue;
-					}
-
-					else if (currentValue % 2 == 1)
-					{
-						oddNumber++;
-						oddNumberSums += currentValue;
-					}
-				}
-
-				 
-			Console.WriteLine("Четных чисел в диапазоне:\n" + evenNumber);
-			Console.WriteLine("Нечетных чисел в диапазоне:\n" + oddNumber);
-
-			Console.WriteLine("Сумма четных чисел: " + evenNumberSums);
-			Console.WriteLine("Сума нечетных чисел: " + oddNumberSums);
+            for (int i = 1; i <= 10; i++)
+            {
+				Console.WriteLine(i);
+            }
 		}
 	}
 }
