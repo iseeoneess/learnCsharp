806aaf6e469929f4571a9e5a0225491c06e39b22
iseeeoneess <iseeone@mail.ru>
2022-03-25 01:48:11 +0300

    array from keyboard :)

--- Program.cs
+++ Program.cs
@@ -6,17 +6,37 @@ namespace LearnCSharp
 	{
 		static void Main(string[] args)
 		{
-			// arrays bitch...
+            // arrays bitch...
 
-			int[] myArray = new int[5];
+            //char [] Myarray = Console.ReadLine().ToArray();
 
-			myArray[0] = 1;
-			
-			int a = myArray[0];
+            //foreach (char i in Myarray)
+            //	Console.WriteLine(i);
 
-			Console.WriteLine(a);
+            Console.WriteLine("Введите кол-во элементов массива");
 
-			Console.ReadLine();
+			int inputArray = int.Parse(Console.ReadLine()); // Создали переменную в которую кладем наше кол-во
+
+			int [] inputArrayValue = new int[inputArray]; // Создали массив, в котором указали кол-во элементов с атрибутом
+            // new int t.e кол-во эл. [и указали нашу переменную]
+
+			for (int i = 0; i < inputArrayValue.Length; i++) 
+                // Создали цикл, который Перебирает наш массив
+                // и заставляет юзера вводить желаемые числа в массив
+            {
+                Console.WriteLine($"\nВведите индекс числа {i + 1}");
+				inputArrayValue[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.Write("\nВывод массива: ");
+            // Использовали foreach, tk он банально проще в синтаксисе.
+            foreach (int i in inputArrayValue)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+
 		}
 	}
 }
