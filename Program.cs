e5a2ea36ac1a6be92127bbbefab0e02c54db3542
iseeeoneess <iseeone@mail.ru>
2022-03-24 23:18:33 +0300

    тернарный оператор "?"

--- Program.cs
+++ Program.cs
@@ -1,98 +1,22 @@
 ﻿using System;
 
-
 namespace LearnCSharp
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Введите h треугольника\n");
-			int height = int.Parse(Console.ReadLine());
 
+			// Ternar operaion (a == b? true : false;)
+			Console.WriteLine("Введите число больше 5");
 
+			int inputNum = int.Parse(Console.ReadLine());
 
-			// normal piramade
+			bool outputNum = inputNum < 5 ? false : true;
 
-			for (int i = 1; i <= height; i++)
-			{
-				for (int j = 1; j <= i; j++)
-				{
-					Console.Write("#");
-				}
-				Console.WriteLine();
-			}
+			Console.WriteLine(outputNum);
 
-
-			for (int i = 0; i < 2; i++)
-			{
-				Console.WriteLine();
-			}
-
-
-
-
-			// reversive piramide..
-			for (int i = 1; i <= height; i++)
-			{
-				for (int j = height; j >= i; j--)
-				{
-					Console.Write("#");
-				}
-				Console.WriteLine();
-			}
-
-
-			for (int i = 0; i < 2; i++)
-			{
-				Console.WriteLine();
-			}
-
-
-			// tabalation piramide..
-			for (int i = 1; i <= height; i++)
-            {
-                for (int j = height; j >= i; j--)
-                {
-					Console.Write(" ");
-                }
-
-                for (int j = 1; j <= i; j++)
-                {
-					Console.Write("##");
-                }
-				Console.WriteLine();
-            }
-
-
-
-
-            for (int i = 0; i < 2; i++)
-			{
-				Console.WriteLine();
-			}
-
-
-
-
-			// tabulation reversive piramide...
-
-			for (int i = 0; i <= height; i++)
-			{
-                for (int j = 0; j <= i; j++)
-                {
-					Console.Write(" ");
-                }
-
-                for (int k = height; k > i; k--)
-                {
-					Console.Write("##");
-                }
-				Console.WriteLine();
-			}
-
-
-
+			Console.ReadLine();
 		}
 	}
 }
