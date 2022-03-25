ae86e9c3460f00994d0aaff7a442a5006b2cb65b
iseeeoneess <iseeone@mail.ru>
2022-03-25 04:25:55 +0300

    arrays last vesrion... tired

--- Program.cs
+++ Program.cs
@@ -2,68 +2,80 @@
 
 namespace LearnCSharp
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Введите кол-во элементов массива");
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите кол-во элементов массива");
 
-            int inputArray = int.Parse(Console.ReadLine()); // Создали переменную в которую кладем наше кол-во
+			int inputArray = int.Parse(Console.ReadLine()); // Создали переменную в которую кладем наше кол-во
 
-            int[] inputArrayValue = new int[inputArray]; // Создали массив, в котором указали кол-во элементов с атрибутом
-                                                         // new int t.e кол-во эл. [и указали нашу переменную]
+			int[] inputArrayValue = new int[inputArray]; // Создали массив, в котором указали кол-во элементов с атрибутом
+														 // new int t.e кол-во эл. [и указали нашу переменную]
 
-            for (int i = 0; i < inputArrayValue.Length; i++)
-            // Создали цикл, который Перебирает наш массив
-            // и заставляет юзера вводить желаемые числа в массив
+			for (int i = 0; i < inputArrayValue.Length; i++)
+			// Создали цикл, который Перебирает наш массив
+			// и заставляет юзера вводить желаемые числа в массив
+			{
+				Console.WriteLine($"\nВведите индекс числа {i + 1}\n");
+				inputArrayValue[i] = int.Parse(Console.ReadLine());
+
+			}
+
+			Console.Write("Вывод массива: \n");
+			// Использовали foreach, tk он банально проще в синтаксисе.
+			for (int i = inputArrayValue.Length - 1; i >= 0; i--)
+			{
+				Console.WriteLine(inputArrayValue[i]);
+			}
+
+			int resultNum = 0;
+
+			Console.Write("\nСумма четных чисел массива: \n");
+
+			for (int i = 0; i < inputArrayValue.Length; i++)
+			{
+				if (inputArrayValue[i] % 2 == 0)
+				{
+					resultNum += inputArrayValue[i];
+				}
+			}
+			Console.Write("\t");
+			Console.Write(resultNum);
+
+
+			int minValue = inputArrayValue[0];
+			Console.Write("\nМинимальное число массива: \n");
+
+            for (int i = 1; i < inputArrayValue.Length; i++)
             {
-                Console.WriteLine($"\nВведите индекс числа {i + 1}\n");
-                inputArrayValue[i] = int.Parse(Console.ReadLine());
-
-            }
-
-            Console.Write("Вывод массива: \n");
-            // Использовали foreach, tk он банально проще в синтаксисе.
-            for (int i = inputArrayValue.Length - 1; i >= 0; i--)
-            {
-                Console.WriteLine(inputArrayValue[i]);
-            }
-
-            int resultNum = 0;
-
-            Console.Write("\nСумма четных чисел массива: \n");
-            for (int i = 0; i < inputArrayValue.Length; i++)
-            {
-                if (inputArrayValue[i] % 2 == 0)
+				if (inputArrayValue[i] < minValue)
                 {
-                    resultNum += inputArrayValue[i];
+                    minValue = inputArrayValue[i];
                 }
-
-                
             }
+			Console.Write("\t");
+            Console.WriteLine(minValue);
 
-            Console.Write(resultNum);
 
+			//for (int i = 0; i < inputArrayValue.Length; i++)
+			//{
+			//    if (inputArrayValue[i] % 2 == 0)
+			//    {
+			//        for (int k = 0; k < inputArrayValue.Length; k++)
+			//        {
+			//            int result = i;
+			//            if (result == k)
+			//            {
+			//                Console.Write($"");
+			//            }
+			//        }
 
+			//    }
+			//}
 
-            //for (int i = 0; i < inputArrayValue.Length; i++)
-            //{
-            //    if (inputArrayValue[i] % 2 == 0)
-            //    {
-            //        for (int k = 0; k < inputArrayValue.Length; k++)
-            //        {
-            //            int result = i;
-            //            if (result == k)
-            //            {
-            //                Console.Write($"");
-            //            }
-            //        }
+			Console.ReadLine();
 
-            //    }
-            //}
-
-            Console.ReadLine();
-
-        }
-    }
+		}
+	}
 }
