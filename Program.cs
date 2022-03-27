0ac1351449e3fa1c6996d91b4d273c0218935c21
iseeeoneess <iseeone@mail.ru>
2022-03-27 21:55:19 +0300

    twice arrays

--- Program.cs
+++ Program.cs
@@ -1,43 +1,22 @@
 ﻿using System;
 using System.Linq;
 
-namespace Arrays
+namespace indexsAndDiapasones
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[,] twiceArray =
+			{
+				{ 2, 5, 10, 11 },
+				{ 99, 36, 41, 52 },
+				{ 91, 75, 34, 41 }
+			};
 
+            Console.WriteLine(twiceArray[1,2]); //41
+		}
+	}
 
-
-            int[] workArray = { 22, 11, 1, 2, 1, 3, 3, 4, 10, 11, 12, 33, 44, 19, 3, 2, 2 };
-
-
-            Array.Sort(workArray);
-
-            for (int i = 0; i < workArray.Length; i++)
-            {
-                Console.WriteLine(workArray[i]);
-            }
-
-            Console.WriteLine("\t" + workArray.Min()); // вывыдит мин число массива
-            Console.WriteLine("\t" + workArray.Max()); // вывыдит макс число массива
-
-            int [] result = workArray.Distinct().ToArray();  // Отсортировали уникальных чисел..
-            for (int i = 0; i < result.Length; i++)
-            {
-                Console.Write($"'{result[i]}' "); // Наше i + '';
-            }
-
-            Console.ReadLine();
-
-
-
-
-
-
-
-
-        }
-    }
 }
+
