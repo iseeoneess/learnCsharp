d9e366f282665ad486863e30dd60f5b101783eae
iseeeoneess <iseeone@mail.ru>
2022-04-01 10:39:11 +0300

    Двумерный массив ввод с клавиатуры

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 
 namespace indexsAndDiapasones
 {
@@ -7,25 +8,59 @@ namespace indexsAndDiapasones
 	{
 		static void Main(string[] args)
 		{
-            int[,] twiceArray =
-            {
-                { 2, 5, 10, 11 },
-                { 99, 36, 41, 52 },
-                { 91, 75, 34, 41 }
-            };
+            Console.WriteLine("Данный массив генерирует случайные числа.\n");
 
-            int height = twiceArray.GetLength(0);
-            int width = twiceArray.GetLength(1);
+            Console.WriteLine("Массив будет представлен в виде таблицы.\n");
 
-            for (int y = 0; y < height; y++)
+
+            Console.WriteLine("Кол-во строк и колонок не должно превышать число 3.\n");
+
+            Console.WriteLine("Введите кол-во строк\n");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите кол-во колонок\n");
+            int width = int.Parse(Console.ReadLine());
+
+            int[,] twiceArray = new int [height, width];
+
+            
+
+            for (int i = 0; i < twiceArray.GetLength(0); i++)
             {
-                Console.WriteLine();
-                for (int x = 0; x < width; x++)
+                for (int j = 0; j < twiceArray.GetLength(1); j++)
                 {
-                    Console.Write(twiceArray[y, x] + "\t");
+                    Console.WriteLine($"Позиция заполнения по Y:{i + 1}, по X: {j + 1}");
+                    twiceArray[i, j] = int.Parse(Console.ReadLine());
                 }
             }
-        }
+
+
+            Console.WriteLine();
+
+
+
+
+
+            for (int i = 0; i < twiceArray.GetLength(0); i++)
+            {
+                if (height >= 4 && width >= 4)
+                {
+                    Console.WriteLine("Вы ввели числа выше допустимых значений.\n");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Закрытие программы\n");
+                    Thread.Sleep(1000);
+                    break;
+                }
+                else {
+                    Console.WriteLine();
+                    for (int j = 0; j < twiceArray.GetLength(1); j++)
+                    {
+                        Console.Write(twiceArray[i, j] + "\t");
+                    }
+                }
+               
+            }
+		}
 	}
 
 }
