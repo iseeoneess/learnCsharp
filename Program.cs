bf5c30ae1e2d4fd875593666338c3ec1dbba52d9
iseeeoneess <iseeone@mail.ru>
2022-04-01 08:49:57 +0300

    Вывод двумерного массива

--- Program.cs
+++ Program.cs
@@ -7,15 +7,25 @@ namespace indexsAndDiapasones
 	{
 		static void Main(string[] args)
 		{
-			int[,] twiceArray =
-			{
-				{ 2, 5, 10, 11 },
-				{ 99, 36, 41, 52 },
-				{ 91, 75, 34, 41 }
-			};
+            int[,] twiceArray =
+            {
+                { 2, 5, 10, 11 },
+                { 99, 36, 41, 52 },
+                { 91, 75, 34, 41 }
+            };
 
-            Console.WriteLine(twiceArray[1,2]); //41
-		}
+            int height = twiceArray.GetLength(0);
+            int width = twiceArray.GetLength(1);
+
+            for (int y = 0; y < height; y++)
+            {
+                Console.WriteLine();
+                for (int x = 0; x < width; x++)
+                {
+                    Console.Write(twiceArray[y, x] + "\t");
+                }
+            }
+        }
 	}
 
 }
