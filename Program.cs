bb76e644a4b4edfa639b3a9985b52a4c6e729aa7
iseeeoneess <iseeone@mail.ru>
2022-04-05 19:23:27 +0300

    Проходной код, ничего нового. Попытка выполнить второе дз (неудачно), т.к я банально не знал о новых фичах. Следующий коммит - рефакторинг данного кода.

--- Program.cs
+++ Program.cs
@@ -2,41 +2,58 @@
 using System.Linq;
 using System.Threading;
 
-namespace functionsAndMetods
+namespace findIndexsFromArrays
 {
-	// Написать метод, который выводит на экран строку.
-	// Символы из которых состит строка и их кол-во вводятся пользователями.
+	// Элемент для поиска индекса в массиве с типом int. Метод должен вернуть индекс первого найденного элемента (если он будет найден)
 	class Program
 	{
-        static void outputResult(string symbol, int iteration)
-        {
-            if (iteration != 0 && iteration > 0)
-            {
-                for (int i = 0; i < iteration; i++)
-                {
-                    Console.Write($" {symbol}");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Ошибка. Введите положительное число больше нуля.");
+		static void Main(string[] args)
+		{
 
+			int[] arr = new int[10];
+
+			Console.WriteLine("\tДанная программа позволяет найти индекс элемента массива\n");
+
+			Console.WriteLine("Введите желаемое число, которое хотите найти в массиве.");
+
+            int indexValue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Программа выведет индекс этого числа.");
+
+            Console.WriteLine("Наш массив: ");
+
+			whileArray(arr);
+
+            Console.WriteLine("\nВаш элемент: ");
+
+			findIndexFromArray(arr, indexValue);
+
+			
+
+			
+		}
+
+		static void whileArray(int [] array)
+		{
+			Random random = new Random();
+			for (int r = 0; r < array.Length; r++)
+			{
+				array[r] = random.Next(100);
+			}
+
+            for (int i = 0; i < array.Length; i++)
+            {
+				Console.Write($"{array[i]} ");
+            }
+		}
+		// Не рабочая функция.
+		static void findIndexFromArray(int [] array,int indexValue)
+        {
+            for (int i = indexValue; i < array.Length; i++)
+            {
+                Console.WriteLine($"Ваше число {array[i]}");
             }
         }
 
-		static void Main(string [] args)
-        {
-            Console.WriteLine("Введите символ или строку");
-
-            string symbol = Console.ReadLine();
-
-            Console.WriteLine("Сколько раз вывести это?");
-
-            int iteration = int.Parse(Console.ReadLine());
-
-            outputResult(symbol, iteration);
-
-            
-        }
 	}
 }
