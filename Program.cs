21158bde2b5e6e04f57251ae81c55fe0e97e4df4
iseeeoneess <iseeone@mail.ru>
2022-04-18 20:50:01 +0300

    created change indexes value

--- Program.cs
+++ Program.cs
@@ -1,39 +1,49 @@
 ﻿using System;
 using System.Threading;
 
-namespace equals
+namespace insertMetod
 {
-	class Progrma
+	class Program
 	{
-
-        // <T> сделали generic метод, ахуеть, это позволяет юзать любой тип данных..
-		static void resizeArray <T> (ref T [] array, int newSize)
+        static void forArrays<T> (T[] arr)
         {
-            T[] newArray = new T [newSize];
-
-            for (int i = 0; i < array.Length && i < newArray.Length; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
-                newArray [i] = array [i];
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+		static void insertMetod<T>(ref T[] arr, int index, T value)
+        {
+            T[] newArray = arr;
+ 
+            if (!(index + 1 > arr.Length))
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    newArray[i] = arr[i];
+                }
+                arr[index] = value;
+            }
+            else
+            {
+                Console.WriteLine("Ты долбаеб, указанный индекс больше, чем есть у массива!");
             }
 
-            array = newArray;
         }
 
 		static void Main(string[] args)
         {
-            int [] firstArray = { 1, 2, 3, 10, 15, 122 };
-            string[] zeliboba = { "hi", "Hello", "Привет", "Nihao", "Namaste" };
+			int [] intArray = { 1, 2, 3, 4 };
+			string [] stringArray = { "hi", "bye", "bia", "si" };
 
-            resizeArray (ref firstArray, 9);
-            resizeArray (ref zeliboba, 3);
+            Program.insertMetod(ref intArray, 3, 3);
+            Program.insertMetod(ref stringArray, 1, "bia");
 
-            for (int i = 0; i < firstArray.Length; i++)
-            {
-                Console.Write(firstArray[i] + " ");
-            }
-            Console.WriteLine();
+            Program.forArrays(intArray);
+            Program.forArrays(stringArray);
 
-            Console.ReadKey();
+            Console.ReadLine();
         }
 	}
 }
