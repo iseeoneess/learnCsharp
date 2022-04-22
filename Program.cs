07f1538a857ea4c394d1710b10f4afe7a67b878d
iseeeoneess <iseeone@mail.ru>
2022-04-23 00:45:42 +0300

    elemenstSum in arrays (recursive metod)

--- Program.cs
+++ Program.cs
@@ -3,46 +3,57 @@ using System.Threading;
 
 
 
-namespace ghoul
+namespace sumarr
 {
-    class Program
-    {
-      static void giveElemenntsInIndexForArrays(int [] arr, int i)
-        {
-           Random r = new Random();
-           arr [i] = r.Next(30);
-           i++;
-           if (i == arr.Length)
-                return;
-           giveElemenntsInIndexForArrays (arr, i);
-        }
+	class Program
+	{
+
+	  static void exampleSumArrays()
+		{
+			int[] randomArr = { 12, 44, 16, 97, 36, 84 };
+
+			int buferResult = 0;
+			int result = 0;
+
+			for (int i = 0; i < randomArr.Length; i++)
+			{
+				buferResult = randomArr[i];
+
+				result += buferResult;
+			}
+			Console.WriteLine(result);
+		}
 
 
 
 
-      static void recursiveMassive(int[] arr, int i)
-        {
-
-            int result = arr[i];
-            Console.WriteLine(result);
-            i++;
-            if (i == arr.Length)
-                return;
-            recursiveMassive(arr, i);
-
-
-        }
-    
-
-      static void Main(string[] args)
-        {
-            int i = 0;
-            int[] arraksahjdkjla = new int[5];
-
-            giveElemenntsInIndexForArrays(arraksahjdkjla, i);
-            recursiveMassive(arraksahjdkjla, i);
+	  static void sumArraysRecursiveMetod (int [] arr, int i, int result, int buferResult)
+		{
+			buferResult = arr[i];
+			result += buferResult;
+			i++;
+			if (i == arr.Length)
+			{
+				Console.WriteLine(result);
+			}
+			if (i == arr.Length)
+				return;
+			sumArraysRecursiveMetod(arr, i, result, buferResult);
 
 
-        }
-    }
+
+
+		}
+
+	  static void Main(string[] args)
+		{
+			int i = 0;
+			int result = 0;
+			int buferResult = 0;
+	  
+
+			int [] randomArr = { 44, 16};
+			sumArraysRecursiveMetod(randomArr, i, result, buferResult);
+		}
+	}
 }
