b3b3d26b6d8ec3e41f56dec9238dfabdb9abb0ee
iseeeoneess <iseeone@mail.ru>
2022-04-30 23:50:26 +0300

    Enum biiitch

--- Program.cs
+++ Program.cs
@@ -2,16 +2,77 @@
 using System.Threading;
 
 
-
-namespace findSumDigit
+namespace enuming
 {
-	class Program
-	{
-		static void Main(string[] args)
+    class Program
+    {
+        enum Countries
         {
-			int a = 1203;
-
-			byte b = (byte)a; // преобразовали грубо говоря инт в байт
+            Russia = 1, 
+            USA,
+            Poland,
+            Japan,
+            UkraineDoggy,
+            UK,
+            Germany
         }
-	}
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите страну из приведеннго списка:");
+
+            var countries = Enum.GetValues(typeof(Countries));
+
+            foreach (var item in countries)
+            {
+                Console.Write(item + " ");
+            }
+
+            
+
+           
+
+            Console.WriteLine("\nВведите желаемую страну: ");
+
+            string inputValueOfCountry = Console.ReadLine();
+
+            Countries country = (Countries)Enum.Parse(typeof(Countries), inputValueOfCountry, ignoreCase: true);
+
+
+            switch (country)
+            {
+                case Countries.Russia:
+                    Console.WriteLine($"You choosed {Countries.Russia}");
+                    break;
+
+                case Countries.USA:
+                    Console.WriteLine($"You choosed {Countries.USA}");
+                    break;
+
+                case Countries.Poland:
+                    Console.WriteLine($"You choosed {Countries.Poland}");
+                    break;
+
+                case Countries.Japan:
+                    Console.WriteLine($"You choosed {Countries.Japan}");
+                    break;
+
+                case Countries.UkraineDoggy:
+                    Console.WriteLine($"You choosed {Countries.UkraineDoggy}");
+                    break;
+
+                case Countries.UK:
+                    Console.WriteLine($"You choosed {Countries.UK}");
+                    break;
+
+                case Countries.Germany:
+                    Console.WriteLine($"You choosed {Countries.Germany}");
+                    break;
+
+                default:
+                    Console.WriteLine("You don't choosed country.");
+                    break;
+            }
+        }
+    }
 }
