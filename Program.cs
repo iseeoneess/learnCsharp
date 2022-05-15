f9f4e630fee6a2bcb06f75273605138a189b22ea
iseeeoneess <iseeone@mail.ru>
2022-05-15 21:24:46 +0300

    Конструктор персонажа (ООП)

--- Program.cs
+++ Program.cs
@@ -1,100 +1,167 @@
 ﻿using System;
 using System.Threading;
 
-namespace carDrive
+namespace mainWork
 {
-	class Car
+	class createCharachter
 	{
-		private int speed = 0;
+		private string birthdayTime = "24.04.2005";
+		private string [] dateBaseForColorHair = {"Red", "Green", "Blue"};
+		private int numerableColorHair;
+		private string outputColorHair;
+		public int sizeHair;
+		public string gender;
+		public string name;
 
+		public void human()
+		{
+			giveInfoForHumanCreator();
 
+			Console.WriteLine("Отлично, у тебя получилось создать персонажа.\n" +
+				"Давай посмотрим, что получилось..\n");
 
-		private void randomNumForGiveInfoOrSpeedInMethod()
+			Console.WriteLine($"Дата рождения: {birthdayTime}\n");
+			Console.WriteLine($"Имя: {name}\n");
+			Console.WriteLine($"Пол: {gender}\n");
+			Console.WriteLine($"Цвет волос: {outputColorHair}\n");
+			Console.WriteLine($"Размер волос: {sizeHair} см\n");
+		}
+
+		private void arrayInfo()
+		{
+            for (int i = 0; i < dateBaseForColorHair.Length; i++)
+            {
+                Console.Write($"Цвет {dateBaseForColorHair[i] } под номером {i + 1} \n");
+            }
+            Console.WriteLine();
+		}
+
+		private void outputInfoForColorHair ()
 		{
 
-			Random random = new Random();
-
-			int randomNum = random.Next(-100, 100);
-
-			if (randomNum > 0)
+			switch (numerableColorHair)
 			{
-				giveSpeedForCar();
-			}
-
-			if (randomNum < 0)
-			{
-				giveMinusSpeedForCar();
-			}
-
-			if (randomNum == 0)
-			{
-				stopSpeedForCar();
+				case 1:
+					outputColorHair = "Red";
+					break;
+				case 2:
+					outputColorHair = "Green";
+					break;
+				case 3:
+					outputColorHair = "Blue";
+					break;
+					
+				default:
+					Console.WriteLine("Error 404!");
+					outputInfoForColorHair();
+					break;
 			}
 		}
 
-
-		public void giveInfoForSpeedCar ()
+		public void giveInfoForHumanCreator ()
 		{
-			randomNumForGiveInfoOrSpeedInMethod();
 
-			if (speed > 0)
+			Console.WriteLine("Привет! Я программа, которая позволит тебе создать своего персонажа\n");
+			Thread.Sleep(1000);
+
+			Console.WriteLine("(частично :=) Пока что я могу составить и вывести только:\n Имя, пол, цвет волос, размер волос\n");
+			Thread.Sleep(1000);
+
+
+
+			giveInfoForNameHuman();
+			chooseGenderForHuman();
+			giveOutputInfoForNumerableColorHair();
+			chooseSizeHair();
+
+
+
+			void giveInfoForNameHuman ()
 			{
-				Console.WriteLine($"\tДвижемся со скоростью {speed} км\\ч");
+				Console.WriteLine("Отлично, введи имя своего персонажа:\n");
+
+				try
+				{
+					name = Console.ReadLine();
+					Thread.Sleep(1000);
+				}
+				catch (Exception)
+				{
+					Console.WriteLine("Ошибка!");
+					giveInfoForNameHuman();
+					throw;
+				}
 			}
 
-			if (speed == 0)
+			void chooseGenderForHuman ()
 			{
-				Console.WriteLine($"\t\nСтоим на месте, т.к скорость {speed} км\\ч");
-			}
+				Console.WriteLine("Теперь пол: \n");
 
-			if (speed < 0)
-			{
-				Console.WriteLine($"\t\nЕдем назад со скоростью {-speed} км\\ч");
-			}
-
+				try
+				{
+					gender = Console.ReadLine();
+					Thread.Sleep(1000);
+				}
+				catch (Exception)
+				{
+					Console.WriteLine("Ошибка!");
+					chooseGenderForHuman();
+					throw;
+				}
 			
+			}
+
+			void giveOutputInfoForNumerableColorHair ()
+			{
+				try
+				{
+					
+					arrayInfo();
+					Console.WriteLine("Выбери нужный цвет под номером");
+					numerableColorHair = int.Parse(Console.ReadLine());
+					outputInfoForColorHair();
+
+				}
+				catch (Exception)
+				{
+					Console.WriteLine("Error, try again!");
+					giveOutputInfoForNumerableColorHair();
+					throw;
+				}
+			}
+
+
+
+			void chooseSizeHair ()
+			{
+				Console.WriteLine("Прекрасненько!\n " +
+				"А теперь выбери размер (до 30см)\n");
+
+				try
+				{
+					sizeHair = int.Parse(Console.ReadLine());
+				}
+				catch (Exception)
+				{
+					if (sizeHair > 30)
+					{
+						Console.WriteLine("Ошибка");
+					}
+					chooseSizeHair();
+					throw;
+				}
+			}
 		}
-
-		private int stopSpeedForCar() 
-		{ 
-
-			Random random = new Random();
-
-			speed = random.Next(0, 1);
-
-			return speed;
-		}
-
-		private int giveSpeedForCar()
-		{
-
-			Random random = new Random();
-
-			speed = random.Next(10);
-
-			return speed;
-		}
-
-		private int giveMinusSpeedForCar ()
-		{
-
-			Random random = new Random();
-
-			speed = random.Next(-10, 0);
-
-			return speed;
-		}
-
 	}
 
 	class Program
 	{
-		static void Main(string [] args)
+		static void Main(string[] args)
 		{
-			var car = new Car();
+			createCharachter createCharachter = new createCharachter();
+			createCharachter.human();
 
-			car.giveInfoForSpeedCar();
+			
 		}
-
 	}
 }
