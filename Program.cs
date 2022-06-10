d6529a1f6c71d870524b16e7d8fd682e2b6fbef9
iseeoneess <iseeone@mail.ru>
2022-06-10 14:00:18 +0300

    квадратные уравнения

--- Program.cs
+++ Program.cs
@@ -1,134 +1,90 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 
-namespace prototypeChance
+namespace twiceEquation
 {
 	class Program
-	{ 
-		static void Main(string [] args)
+	{
+		static void Main(string[] args)
 		{
 			Console.Title = "Квадратные уравнения";
 			Console.ForegroundColor = ConsoleColor.Green;
 
 			Console.WriteLine("Данная программа решает простые квадратные уравнения.\n");
 
-            randomNum = r.Next(10);
-            return;
-        }
+			Console.WriteLine("Вводите поочердно то, что требует программа.\n");
+
+			Console.WriteLine("Напоминаю как выглядит квадратное уравнение:\n");
+
+			Console.WriteLine("Пример: x^2 - 10x + 6 = 0");
+
+			Console.WriteLine("\tГде x^2 - а, 10x - b, 6 - c\n");
+
+			Console.WriteLine("Когда будете вводить a и b, учитывайте, что программа автоматически ставит x.\n");
+
+			Console.WriteLine("Введите a: ");
+
+			Console.WriteLine("\tЕсли вы хотите, чтобы a было в виде x^2, то напишите 0");
+			int a = int.Parse(Console.ReadLine());
+			if (a == 0)
+			{
+				a = 1;
+			}
+			Console.WriteLine("Введите первый знак (+ или -) : ");
+			char firstSign = char.Parse(Console.ReadLine());
+			Console.WriteLine("Введите b: ");
+			int b = int.Parse(Console.ReadLine());
+			if (firstSign == '-')
+			{
+				b = -b;
+			}
+			Console.WriteLine("Введите второй знак (+ или -): ");
+			char secondSign = char.Parse(Console.ReadLine());
+			Console.WriteLine("Введите c");
+			int c = int.Parse(Console.ReadLine());
+			if (secondSign == '-')
+			{
+				c = -c;
+			}
+			Console.WriteLine("Введите то, что будет после равно");
+			int equals = int.Parse(Console.ReadLine());
 
 
-        public void indexQuestion()
-        {
-            generateRandomNum();
-            giveNumberForAnswer = randomNum;
 
-            Random r = new Random();
+			Console.WriteLine($"\tВаше уравнение: {a}x^2 {firstSign} {b}x {secondSign} {c} = {equals}");
+			while (true)
+			{
+				int square = Convert.ToInt32(Math.Pow(b, 2));
 
-            int indexRandomNum = r.Next(10);
+				int discriminanteFormula = square - 4 * a * c;
 
-            if (giveNumberForAnswer > 5)
-            {
-                switch (indexRandomNum)
-                {
-                    case 0:
-                        Console.WriteLine("Да!");
-                        break;
-                    case 1:
-                        Console.WriteLine("Определенно!");
-                        break;
-                    case 2:
-                        Console.WriteLine("Безусловно!");
-                        break;
-                    case 3:
-                        Console.WriteLine("Оно того стоит!");
-                        break;
-                    case 4:
-                        Console.WriteLine("Скорее да, чем нет!");
-                        break;
-                    case 5:
-                        Console.WriteLine("Конечно!");
-                        break;
-                    case 6:
-                        Console.WriteLine("Попробуй");
-                        break;
-                    case 7:
-                        Console.WriteLine("Несомненно");
-                        break;
-                    case 8:
-                        Console.WriteLine("Дерзай");
-                        break;
-                    case 9:
-                        Console.WriteLine("100%");
-                        break;
-                    case 10:
-                        Console.WriteLine("Лучшее решение в твоей жизни!");
-                        break;
-                    default:
-                        break;
-                }
+				int squareDiscriminanteFormula = Convert.ToInt32(Math.Sqrt(discriminanteFormula));
 
-            }
+				if (discriminanteFormula > 0)
+				{
+					double resultX1 = (-b + squareDiscriminanteFormula) / (2 * a);
+					double resultX2 = (-b - squareDiscriminanteFormula) / (2 * a);
 
-            else if (giveNumberForAnswer < 5)
-            {
-                switch (indexRandomNum)
-                {
-                    case 0:
-                        Console.WriteLine("Нет!");
-                        break;
-                    case 1:
-                        Console.WriteLine("Ни в коем случае!");
-                        break;
-                    case 2:
-                        Console.WriteLine("Не смей");
-                        break;
-                    case 3:
-                        Console.WriteLine("Не стоит");
-                        break;
-                    case 4:
-                        Console.WriteLine("Мне кажется нет");
-                        break;
-                    case 5:
-                        Console.WriteLine("Попробуй другое");
-                        break;
-                    case 6:
-                        Console.WriteLine("Не делай этого!");
-                        break;
-                    case 7:
-                        Console.WriteLine("Лучше сделай наоборот");
-                        break;
-                    case 8:
-                        Console.WriteLine("Never!");
-                        break;
-                    case 9:
-                        Console.WriteLine("Тяжело говорить, но нет");
-                        break;
-                    case 10:
-                        Console.WriteLine("Не сегодня");
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-    }
+					Console.WriteLine($"Первый корень: {resultX1}");
+					Console.WriteLine($"Второй корень: {resultX2}");
+					break;
+				}
+				if (discriminanteFormula < 0)
+				{
+					Console.WriteLine("Корней нет.");
+					break;
+				}
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Write("Напишите насущный вопрос о котором вы долго думаете ");
-            Console.Write("и не можете принять решение\n");
+				if (discriminanteFormula == 0)
+				{
+					double resultX1 = (-b + squareDiscriminanteFormula) / (2 * a);
 
-            string inputQuestion = Console.ReadLine();
-
-            GiveAnswer answer = new GiveAnswer();
-
-            answer.indexQuestion();
-        }
-    }
+					Console.WriteLine($"Т.к корень дискриминанта равен нулю, то и корень будет 1: {resultX1}");
+					break;
+				}
+			}
+			Console.ReadLine();
+		}
+	}
 }
-
-
-
-
